41cb6b2ac9d05f10374a028bf13bd79a788fb4c2
tomneo2004 <tomneo2004@gmail.com>
2024-02-13 12:05:38 +0800

    feat(home): add title and subtitle
    add title and subtitle

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,44 @@
 import * as React from "react";
-import { Button } from "@mui/material";
+import { Typography } from "@mui/material";
 import CommonLayout from "../layouts/common-layout";
 import Seo from "../components/seo/seo";
+import { useInView } from "react-intersection-observer";
 
 const IndexPage = () => {
+  const titleInView = useInView({
+    delay: 1000,
+    triggerOnce: true,
+    trackVisibility: true,
+  });
+  const subtitleInView = useInView({
+    delay: 1000,
+    triggerOnce: true,
+    trackVisibility: true,
+  });
   return (
     <CommonLayout>
-      <Button variant="contained">Hello</Button>
+      <div ref={titleInView.ref} className="overflow-hidden">
+        <Typography
+          className={`transition ease-out duration-[800ms] ${
+            titleInView.inView ? "translate-y-0" : "translate-y-10"
+          }`}
+          variant="h2"
+          align="center"
+        >
+          Hello
+        </Typography>
+      </div>
+      <div ref={subtitleInView.ref} className="overflow-hidden">
+        <Typography
+          className={`transition ease-out delay-[800ms] duration-[800ms] ${
+            subtitleInView.inView ? "translate-y-0" : "translate-y-10"
+          }`}
+          variant="h3"
+          align="center"
+        >
+          Wellcome
+        </Typography>
+      </div>
     </CommonLayout>
   );
 };
