82e9a9b900a05387e21476136037317099a3ff6b
tomneo2004 <tomneo2004@gmail.com>
2024-02-15 11:30:15 +0800

    refactor(home): use RenderInView component
    use RenderInView component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,37 +2,36 @@ import * as React from "react";
 import { Typography } from "@mui/material";
 import CommonLayout from "../layouts/common-layout";
 import Seo from "../components/seo/seo";
-import { useInView } from "react-intersection-observer";
 import SpinText from "../components/spin-text/spin-text";
 import { MUIThemeContext } from "../components/mui-theme/mui-theme-provider";
+import RenderInView from "../components/render-in-view/render-in-view";
 
 const IndexPage = () => {
   const { theme } = React.useContext(MUIThemeContext);
   const letterSpinColor = theme.palette.spinLetter.main;
-  const titleInView = useInView({
-    delay: 1000,
-    triggerOnce: true,
-    trackVisibility: true,
-  });
   return (
     <CommonLayout>
-      <div ref={titleInView.ref} className="overflow-hidden">
-        {titleInView.inView ? (
-          <Typography className="leading-loose" variant="h3" align="center">
-            <SpinText
-              text="HELLO"
-              duration={350}
-              randLetterColor={letterSpinColor}
-            />
-            <SpinText
-              text="WELCOME"
-              duration={300}
-              delay={1600}
-              randLetterColor={letterSpinColor}
-            />
-          </Typography>
-        ) : null}
-      </div>
+      <RenderInView
+        options={{
+          delay: 1000,
+          triggerOnce: true,
+          trackVisibility: true,
+        }}
+      >
+        <Typography className="leading-loose" variant="h3" align="center">
+          <SpinText
+            text="HELLO"
+            duration={350}
+            randLetterColor={letterSpinColor}
+          />
+          <SpinText
+            text="WELCOME"
+            duration={300}
+            delay={1600}
+            randLetterColor={letterSpinColor}
+          />
+        </Typography>
+      </RenderInView>
     </CommonLayout>
   );
 };
